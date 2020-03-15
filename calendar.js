060d3773a7a0681ae2f478d2de4b0e28fcf0e238
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-03-15 12:39:04 -0400

    changing everything

--- calendar.js
+++ calendar.js
@@ -1,18 +1,31 @@
-$(document).ready(function(){
+var hours = ["9:00 A.M.", "10:00 A.M.", "11:00 A.M.", "12:00 P.M.", "1:00 P.M.", "2:00 P.M.", "3:00 P.M.", "4:00 P.M.", "5:00 P.M."]
 
-//var save = $(".saveBtn");  
+$(document).ready(function () {
+    for (i = 0; i < 9; i++) {
+        var div = $("<div>");
+        div.attr("class", "row");
+        var input = $("<input>");
+        var span = $("<span>");
+        span.text (hours[i]);
+        var button = $("<button>");
 
-//for (var i = 0; i < save.length; i++)
+        if (localStorage.getItem(i)) {
+        input.val(localStorage.getItem(i));
+        }
 
-    $(".saveBtn").on("click", function(event){
-    event.preventDefault();
+        $(button).attr("data-ref", i);
+        input.attr("id", i);
 
-    var userinput = $(".textarea");
-    var eventplacement = $(".events");
-        
-        eventplacement.text(userinput);
+        button.click(function (){
+        var i = $(this).attr("data-ref");
+        var text = $("#" + i).val();
+        localStorage.setItem(i, text);
+        });
 
-    });
+        div.append(span, input, button);
+        $(".container").append(div);
+    }
+});
 
     
 
@@ -31,4 +44,3 @@ $(document).ready(function(){
 
 
 
-});
