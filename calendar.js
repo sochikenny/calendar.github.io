faa6b26547786ba4c931da7abd043b1032151f31
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-03-15 13:36:25 -0400

    more things

--- calendar.js
+++ calendar.js
@@ -1,22 +1,24 @@
 var hours = ["9:00 A.M.", "10:00 A.M.", "11:00 A.M.", "12:00 P.M.", "1:00 P.M.", "2:00 P.M.", "3:00 P.M.", "4:00 P.M.", "5:00 P.M."]
 
 $(document).ready(function () {
-    for (i = 0; i < 9; i++) {
+    for (var i = 0; i < 9; i++) {
         var div = $("<div>");
         div.attr("class", "row");
+
         var input = $("<input>");
+        input.attr("id", i);
+
         var span = $("<span>");
         span.text (hours[i]);
+        
         var button = $("<button>");
+        button.attr("data-ref", i);
 
         if (localStorage.getItem(i)) {
         input.val(localStorage.getItem(i));
         }
 
-        $(button).attr("data-ref", i);
-        input.attr("id", i);
-
-        button.click(function (){
+        button.click(function(){
         var i = $(this).attr("data-ref");
         var text = $("#" + i).val();
         localStorage.setItem(i, text);
