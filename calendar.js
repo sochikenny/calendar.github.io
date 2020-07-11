c072c641820883898653b71fb983e894786bea3a
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-07-11 01:39:11 -0400

    minor tweak

--- calendar.js
+++ calendar.js
@@ -1,17 +1,17 @@
-var hours = ["9:00 A.M.", "10:00 A.M.", "11:00 A.M.", "12:00 P.M.", "1:00 P.M.", "2:00 P.M.", "3:00 P.M.", "4:00 P.M.", "5:00 P.M."]
+const hours = ["9:00 A.M.", "10:00 A.M.", "11:00 A.M.", "12:00 P.M.", "1:00 P.M.", "2:00 P.M.", "3:00 P.M.", "4:00 P.M.", "5:00 P.M."]
 
 $(document).ready(function () {
-    for (var i = 0; i < 9; i++) {
-        var div = $("<div>");
+    for (let i = 0; i < 9; i++) {
+        const div = $("<div>");
         div.attr("class", "row");
 
-        var span = $("<span>");
+        const span = $("<span>");
         span.text (hours[i]);
 
-        var input = $("<input>");
+        const input = $("<input>");
         input.attr("id", i);
 
-        var button = $("<button>");
+        const button = $("<button>");
         button.attr("data-ref", i);
         button.text("Save");
 
@@ -20,15 +20,20 @@ $(document).ready(function () {
         }
 
         button.click(function(){
-        var i = $(this).attr("data-ref");
-        var text = $("#" + i).val();
+        const i = $(this).attr("data-ref");
+        const text = $("#" + i).val();
         localStorage.setItem(i, text);
         });
 
         div.append(span, input, button);
         $(".container").append(div);
-    }
+
+}
+
+
 });
+
+
 
     
 
